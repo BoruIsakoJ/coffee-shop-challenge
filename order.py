fdc2ae1ecc567615cef12d0ad2f68f3dc7fe8404
Boru Isako <isco.jaldy@gmail.com>
2025-05-27 10:48:48 +0300

    Refactor Order class to raise ValueError for invalid customer, coffee, and price inputs

--- order.py
+++ order.py
@@ -3,16 +3,19 @@ from coffee import Coffee
 
 class Order:
     def __init__(self, customer, coffee, price):
-        if not isinstance(customer, Customer):
-            raise Exception("Customer must be an instance of Customer")
-        if not isinstance(coffee, Coffee):
-            raise Exception("Coffee must be an instance of Coffee")
-        if not type(price)== float or not (1.0 <= price <= 10.0):
-            raise Exception("Price must be a float between 1.0 and 10.0")        
+        if isinstance(customer, Customer):
+            self._customer = customer
+        else:
+            raise ValueError("Customer must be an instance of Customer")
+        if isinstance(coffee, Coffee):
+            self._coffee = coffee
+        else:
+            raise ValueError("Coffee must be an instance of Coffee")
+        if isinstance(price, (float, int)) and (1.0 <= price <= 10.0):
+            self._price = float(price)
+        else:
+            raise ValueError("Price must be a float between 1.0 and 10.0")        
         
-        self._customer = customer
-        self._coffee = coffee
-        self._price = price
         customer._orders.append(self)
         coffee._orders.append(self)
         
@@ -28,18 +31,27 @@ class Order:
     def price(self):
         return self._price
     
-    
-customer1 = Customer("Boru")
-coffee1 = Coffee("Arabica")
-order1 = customer1.create_order(coffee1, 3.5)
 
-customer2 = Customer("Isako")
-order2 = customer2.create_order(coffee1, 9.5)
+if __name__ == "__main__":
+    try:
+        customer1 = Customer("Boru")
+        coffee1 = Coffee("Capuccino")
+        order1 = customer1.create_order(coffee1, 3.5)
+        
 
-print("Customer Name:",customer1.name)  
-print("Coffee Name:",coffee1.name)  
-print("Coffee Price:",order1.price)  
-print("Customer Orders:",customer1.orders())  
-print("No. of orders:",coffee1.num_orders())  
-print("Average price",coffee1.average_price())  
-print("Customers:",coffee1.customers())   
+        customer2 = Customer("Isako")
+        order2 = customer2.create_order(coffee1, 2.5)
+        order3 = customer2.create_order(coffee1, 8.5)
+
+        print("Customer Name:", customer2.name)
+        print("Coffee Name:", coffee1.name)
+        print("Coffee Price:", order2.price)
+        print("Customer Orders:", customer2.orders)
+        print("No. of orders:", coffee1.num_orders())
+        print("Average price", coffee1.average_price())
+        print("Customers:", coffee1.customers())
+        print("Top Customer for this coffee:", Customer.most_aficionado(coffee1).name)
+    except ValueError as e:
+        print("Error during object creation:", e)
+    except AttributeError as e:
+        print("Attribute error:", e)
